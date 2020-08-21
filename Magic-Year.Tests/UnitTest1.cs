992b806fe7fe94230584b8a8bc12817616370739
GorginZ <georgia.z.leng@gmail.com>
2020-08-21 16:23:53 +1000

    Made test for making fullname variable concating first and last name inputs and wrote code to pass test

---
+++ Magic-Year.Tests/UnitTest1.cs
@@ -0,0 +1,17 @@
+using System;
+using Xunit;
+using Magic_Year;
+
+namespace Magic_Year.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void FullName()
+        { 
+          var testFullName = Program.NameConcat("Georgia", "Leng");
+          Assert.Equal("Georgia Leng", testFullName);
+        }
+
+    }
+}
---
+++ Magic-Year/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Magic_Year
+{
+  public class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Hello Welcome to magic year thing!");
+      Console.WriteLine("please enter your first name");
+      var firstName = Console.ReadLine();
+      Console.WriteLine("Please enter your last name");
+      var lastName = Console.ReadLine();
+      Console.WriteLine(NameConcat(firstName, lastName));
+    }
+
+    public static string NameConcat(string firstName, string lastName) => $"{firstName} {lastName}";
+
+  }
+}
