69cb75f08b69a58f145decc1809499bdf53200d0
GorginZ <georgia.z.leng@gmail.com>
2020-08-24 14:27:52 +1000

    testing validations

--- Magic-Year/Program.cs
+++ Magic-Year/Program.cs
@@ -12,7 +12,18 @@ namespace Magic_Year
     { 
    
       List<string> inputQuestions = new List<string>{ "Please enter your first name: ", "Please enter your Last name: ", "Please enter your annual salary: ", "please enter your work start year: " };
-      
+
+      var testInput = new UserInput();
+      Console.WriteLine(inputQuestions[0]);
+      testInput.FirstName = Console.ReadLine();
+      Console.WriteLine(inputQuestions[1]);
+      testInput.LastName = Console.ReadLine();
+      Console.WriteLine(inputQuestions[2]);
+      testInput.AnnualSalary = Console.ReadLine();
+      Console.WriteLine(inputQuestions[3]);
+      testInput.WorkStartYear = Console.ReadLine();
+      Console.WriteLine($"{testInput.FirstName} {testInput.LastName} {testInput.AnnualSalary} {testInput.WorkStartYear}");
+
     }
 
   }
--- Magic-Year/UserInput.cs
+++ Magic-Year/UserInput.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Text.RegularExpressions;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.Text;
 
@@ -9,62 +10,39 @@ namespace Magic_Year
 {
   public class UserInput
   {
-      [Required, RegularExpression("/^[A-Za-z]+$/")]
+    [Required, RegularExpression("/^[A-Za-z]+$/")  ]
 
-      public string FirstName { get; set; }
+    public string FirstName { get; set; }
 
 
-       [Required, RegularExpression("/^[A-Za-z]+$/")]
-     
-      public string LastName { get; set; }
+    [Required, RegularExpression("/^[A-Za-z]+$/")]
+
+    public string LastName { get; set; }
     [Required]
-      public string AnnualSalary { get; set; }
-      [Required]
-      public decimal WorkStartYear { get; set; }
-       
-      public static string NameConcat(string FirstName, string LastName) => $"{FirstName} {LastName}";
+    public string AnnualSalary { get; set; }
+    [Required]
+    public string WorkStartYear { get; set; }
 
-      
+    public static string NameConcat(string FirstName, string LastName) => $"{FirstName} {LastName}";
+
+
     public static string MonthlySal(string annual)
     {
       int annualInt;
       int monthly;
       var resBool = int.TryParse(annual, out annualInt);
 
-      if (resBool == true)
-      {
-        monthly = (annualInt / 12);
-        return $"Monthly Salary: {monthly}";
-
-      }
-      else
-      {
-        throw new Exception("Annual Salary Should only include digits, eg: 60000");
-      }
+      monthly = (annualInt / 12);
+      return $"Monthly Salary: {monthly}";
     }
 
-        public static string CalcMagicYear(string startYear)
+    public static string CalcMagicYear(string startYear)
     {
       int magicYear;
       int startInt;
       var resBool = int.TryParse(startYear, out startInt);
-
-      if (resBool == true)
-      {
-        magicYear = (startInt + 65);
-        return $"Magic Year: {magicYear}";
-      }
-
-      else
-      {
-        throw new Exception("Start Year Should only include digits, eg: 1980");
-      }
-
+      magicYear = (startInt + 65);
+      return $"Magic Year: {magicYear}";
     }
-    
   }
-
-
-
 }
-  
