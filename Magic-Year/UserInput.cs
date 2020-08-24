6ab739c5a63fd77bdb811e23113bb12fc55d58ec
GorginZ <georgia.z.leng@gmail.com>
2020-08-24 14:36:10 +1000

    adding validations for first and last name

--- Magic-Year/UserInput.cs
+++ Magic-Year/UserInput.cs
@@ -10,17 +10,22 @@ namespace Magic_Year
 {
   public class UserInput
   {
-    [Required, RegularExpression("/^[A-Za-z]+$/")  ]
-
+ [Required(ErrorMessage="First Name Required")]  
+       [MaxLength(15,ErrorMessage="First Name should not more than 1`5 character")]  
+       [MinLength(2,ErrorMessage="First Name should be more than 2 characters")]   
     public string FirstName { get; set; }
 
 
-    [Required, RegularExpression("/^[A-Za-z]+$/")]
-
+    // [Required, RegularExpression("/^[A-Za-z]+$/")]
+[Required(ErrorMessage="Last Name Required")]  
+       [MaxLength(15,ErrorMessage="Last Name should not more than 1`5 character")]  
+       [MinLength(2,ErrorMessage="Last Name should be more than 2 characters")] 
     public string LastName { get; set; }
     [Required]
+    [RegularExpression("^[0-9]+$", ErrorMessage = "Annual Salary must include numbers only")]   
     public string AnnualSalary { get; set; }
     [Required]
+    [RegularExpression("^[0-9]+$", ErrorMessage = "Your work start your should only include numbers")]   
     public string WorkStartYear { get; set; }
 
     public static string NameConcat(string FirstName, string LastName) => $"{FirstName} {LastName}";
